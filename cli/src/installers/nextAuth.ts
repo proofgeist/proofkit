34a9dbb7b3c0b2256c0d053df139023b2f5fcfc6
Eric Luce <37158449+eluce2@users.noreply.github.com>
2024-09-26 17:07:01 -0500

    inject user buttons to header slots

--- cli/src/installers/nextAuth.ts
+++ cli/src/installers/nextAuth.ts
@@ -1,5 +1,12 @@
 import path from "path";
 import fs from "fs-extra";
+import {
+  JsxOpeningFragment,
+  Project,
+  SyntaxKind,
+  type ReturnStatement,
+  type SourceFile,
+} from "ts-morph";
 
 import { PKG_ROOT } from "~/consts.js";
 import { runExecCommand } from "~/helpers/installDependencies.js";
@@ -62,6 +69,41 @@ export const nextAuthInstaller = async ({
     path.join(projectDir, "src/app/auth")
   );
 
+  // copy auth components
+  fs.copySync(
+    path.join(extrasDir, "src/components/next-auth"),
+    path.join(projectDir, "src/components/next-auth")
+  );
+
+  const project = new Project({
+    tsConfigFilePath: path.join(projectDir, "tsconfig.json"),
+  });
+
+  // modify root layout to wrap with session provider
+  addNextAuthProviderToRootLayout(
+    project.addSourceFileAtPath(path.join(projectDir, "src/app/layout.tsx"))
+  );
+
+  // inject signin/signout components to header slots
+  addToHeaderSlot(
+    project.addSourceFileAtPath(
+      path.join(projectDir, "src/components/AppShell/slot-header-right.tsx")
+    ),
+    "@/components/next-auth/user-menu"
+  );
+  addToHeaderSlot(
+    project.addSourceFileAtPath(
+      path.join(
+        projectDir,
+        "src/components/AppShell/slot-header-mobile-content.tsx"
+      )
+    ),
+    "@/components/next-auth/user-menu-mobile"
+  );
+
+  // add a protected safe-action-client
+
+  // TODO do this part in-house, maybe with execa directly
   await runExecCommand({
     command: ["auth", "secret"],
     projectDir,
@@ -80,3 +122,116 @@ export const nextAuthInstaller = async ({
     ],
   });
 };
+
+function addNextAuthProviderToRootLayout(rootLayoutSource: SourceFile) {
+  // Add imports
+  rootLayoutSource.addImportDeclaration({
+    namedImports: [{ name: "NextAuthProvider" }],
+    moduleSpecifier: "@/components/next-auth/next-auth-provider",
+  });
+  rootLayoutSource.addImportDeclaration({
+    namedImports: [{ name: "auth" }],
+    moduleSpecifier: "@/server/auth",
+  });
+
+  const exportDefault = rootLayoutSource.getFunction((dec) =>
+    dec.isDefaultExport()
+  );
+
+  // make the function async
+  exportDefault?.setIsAsync(true);
+
+  // get the session server-side
+  exportDefault
+    ?.getFirstDescendantByKind(SyntaxKind.Block)
+    ?.insertStatements(0, "const session = await auth();");
+
+  // get the body element from the return statement
+  const bodyElement = exportDefault
+    ?.getBody()
+    ?.getFirstDescendantByKind(SyntaxKind.ReturnStatement)
+    ?.getDescendantsOfKind(SyntaxKind.JsxOpeningElement)
+    .find(
+      (openingElement) => openingElement.getTagNameNode().getText() === "body"
+    )
+    ?.getParentIfKind(SyntaxKind.JsxElement);
+
+  // wrap the body element with the next auth provider
+  bodyElement?.replaceWithText(
+    `<NextAuthProvider session={session}>
+      ${bodyElement.getText()}
+    </NextAuthProvider>`
+  );
+
+  rootLayoutSource.formatText();
+  rootLayoutSource.saveSync();
+}
+
+function addToHeaderSlot(slotSourceFile: SourceFile, importFrom: string) {
+  slotSourceFile.addImportDeclaration({
+    defaultImport: "UserMenu",
+    moduleSpecifier: importFrom,
+  });
+
+  // ensure Group from @mantine/core is imported
+  const mantineCoreImport = slotSourceFile.getImportDeclaration(
+    (dec) => dec.getModuleSpecifierValue() === "@mantine/core"
+  );
+  if (!mantineCoreImport) {
+    slotSourceFile.addImportDeclaration({
+      namedImports: [{ name: "Group" }],
+      moduleSpecifier: "@mantine/core",
+    });
+  } else {
+    const groupImport = mantineCoreImport
+      .getNamedImports()
+      .find((imp) => imp.getName() === "Group");
+
+    if (!groupImport) {
+      mantineCoreImport.addNamedImport({ name: "Group" });
+    }
+  }
+
+  const returnStatement = ensureReturnStatementIsWrappedInFragment(
+    slotSourceFile
+      .getFunction((dec) => dec.isDefaultExport())
+      ?.getBody()
+      ?.getFirstDescendantByKind(SyntaxKind.ReturnStatement)
+  );
+
+  const existingElements = returnStatement
+    ?.getFirstDescendantByKind(SyntaxKind.JsxOpeningFragment)
+    ?.getParentIfKind(SyntaxKind.JsxFragment)
+    ?.getFirstDescendantByKind(SyntaxKind.SyntaxList)
+    ?.getText();
+
+  if (!existingElements) {
+    console.log(
+      `Failed to inject into header slot at ${slotSourceFile.getFilePath()}`
+    );
+    return;
+  }
+
+  returnStatement?.replaceWithText(
+    `return (<><Group>${existingElements}<UserMenu /></Group></>)`
+  );
+  returnStatement?.formatText();
+  slotSourceFile.saveSync();
+}
+
+// TODO move to utils
+function ensureReturnStatementIsWrappedInFragment(
+  returnStatement: ReturnStatement | undefined
+) {
+  const expression =
+    returnStatement
+      ?.getExpressionIfKind(SyntaxKind.ParenthesizedExpression)
+      ?.getExpression() ?? returnStatement?.getExpression();
+
+  if (expression?.isKind(SyntaxKind.JsxFragment)) {
+    return returnStatement;
+  }
+
+  returnStatement?.replaceWithText(`return <>${expression}</>;`);
+  return returnStatement;
+}

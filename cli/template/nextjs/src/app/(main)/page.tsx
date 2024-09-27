2f4b143f445cac7279202c4a5622e1e215cd26cb
Eric Luce <37158449+eluce2@users.noreply.github.com>
2024-09-27 15:41:37 -0500

    update home page template

--- cli/template/nextjs/src/app/(main)/page.tsx
+++ cli/template/nextjs/src/app/(main)/page.tsx
@@ -1,6 +1,83 @@
-import { Text } from "@mantine/core";
-import Link from "next/link";
+import {
+  ActionIcon,
+  Anchor,
+  AppShellFooter,
+  Box,
+  Code,
+  Container,
+  Group,
+  px,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import { IconBrandGithub, IconExternalLink } from "@tabler/icons-react";
+import Image from "next/image";
 
 export default function Home() {
-  return <Text>Welcome!</Text>;
+  return (
+    <>
+      <Container mt="5rem">
+        <Stack gap="xl" ta="center">
+          <Image
+            style={{ marginRight: "auto", marginLeft: "auto" }}
+            src="https://raw.githubusercontent.com/proofgeist/proofkit/320a74c7d5597013a3f344218f8cc9fe3802e2d1/www/public/images/proofkit.svg"
+            alt="ProofKit"
+            width={500}
+            height={300}
+          />
+          <Title order={1}>Welcome!</Title>
+
+          <Text style={{ textWrap: "balance" }}>
+            This is the base template home page. To add more pages, components,
+            or other features, run the ProofKit CLI from within your project.
+          </Text>
+          <Code block>pnpm proofkit</Code>
+          <Box>
+            <Anchor
+              href="https://proofkit.proofgeist.com"
+              target="_blank"
+              rel="proofkit-app"
+              style={{ display: "inline-flex", alignItems: "center", gap: 4 }}
+            >
+              ProofKit Docs <IconExternalLink size={px("1rem")} />
+            </Anchor>
+          </Box>
+        </Stack>
+      </Container>
+      <AppShellFooter withBorder={false} mb="1rem">
+        <Container h="100%">
+          <Group justify="space-between">
+            <Group>
+              <Text size="sm" c="dimmed">
+                Sponsored by{" "}
+                <Anchor
+                  href="https://proofgeist.com"
+                  target="_blank"
+                  rel="proofkit-app"
+                >
+                  Proof+Geist
+                </Anchor>{" "}
+                and{" "}
+                <Anchor
+                  href="https://ottomatic.cloud"
+                  target="_blank"
+                  rel="proofkit-app"
+                >
+                  Ottomatic
+                </Anchor>
+              </Text>
+            </Group>
+            <Group>
+              <a href="https://github.com/proofgeist/proofkit" target="_blank">
+                <ActionIcon variant="subtle" color="gray" size="lg">
+                  <IconBrandGithub size={px("1.25rem")} />
+                </ActionIcon>
+              </a>
+            </Group>
+          </Group>
+        </Container>
+      </AppShellFooter>
+    </>
+  );
 }

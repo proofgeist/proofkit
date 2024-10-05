ae4a02b3bb49b317a1a569c01c77b0cdc2aafb35
Eric Luce <37158449+eluce2@users.noreply.github.com>
2024-10-05 14:13:22 -0500

    basic docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "ProofKit CLI",
+      social: {
+        github: "https://github.com/proofgeist/kit",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            { label: "Getting Started", slug: "guides/getting-started" },
+            { label: "Project Structure", slug: "guides/folder-structure" },
+          ],
+        },
+        {
+          label: "Templates",
+          autogenerate: { directory: "templates" },
+        },
+      ],
+    }),
+  ],
+
+  output: "server",
+  adapter: vercel(),
 });

e97a8fe08b37c3a94daf41c45414981d722ba53d
Eric Luce <37158449+eluce2@users.noreply.github.com>
2025-02-19 14:28:28 -0600

    fix eslint errors

--- cli/template/extras/fmaddon-auth/app/auth/verify-email/page.tsx
+++ cli/template/extras/fmaddon-auth/app/auth/verify-email/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
 
   // TODO: Ideally we'd sent a new verification email automatically if the previous one is expired,
   // but we can't set cookies inside server components.
-  let verificationRequest = await getUserEmailVerificationRequestFromRequest();
+  const verificationRequest = await getUserEmailVerificationRequestFromRequest();
   if (verificationRequest === null && user.emailVerified) {
     const redirectTo = await getRedirectCookie();
     return redirect(redirectTo);

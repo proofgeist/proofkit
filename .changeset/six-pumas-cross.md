d0627b2633619c40ec3d43f5733b82aebfe94fe6
Eric Luce <37158449+eluce2@users.noreply.github.com>
2025-03-24 16:39:55 -0500

    update base package versions

---
+++ .changeset/six-pumas-cross.md
@@ -0,0 +1,5 @@
+---
+"@proofgeist/kit": patch
+---
+
+update base package versions
--- cli/template/vite-wv/src/routes/__root.tsx
+++ cli/template/vite-wv/src/routes/__root.tsx
@@ -1,6 +1,5 @@
-import * as React from "react";
-import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: RootComponent,

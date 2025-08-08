9f4c30b352a5b5a0317ac449697224f9e724e425
Eric Luce <37158449+eluce2@users.noreply.github.com>
2025-08-08 14:35:09 -0500

    Release version 0.2.4 of @proofkit/better-auth, adding auto loading of environment variables in the migrate CLI.

--- packages/better-auth/CHANGELOG.md
+++ packages/better-auth/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @proofkit/better-auth
 
+## 0.2.4
+
+### Patch Changes
+
+- Auto load env vars in migrate CLI
+
 ## 0.2.3
 
 ### Patch Changes
--- packages/better-auth/src/cli/index.ts
+++ packages/better-auth/src/cli/index.ts
@@ -14,6 +14,7 @@ import prompts from "prompts";
 import chalk from "chalk";
 import { AdapterOptions } from "../adapter";
 import { createFmOdataFetch } from "../odata";
+import "dotenv/config";
 
 async function main() {
   const program = new Command();
